e5373fb779d2580847961a1ce4b33270829c914e
Mankaran Singh <mankaransingh39@gmail.com>
2024-05-28 18:43:02 +0530

    Added: WhyUs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,14 @@ import Blogs from "./(homeComponent)/Blogs";
 import Cross from "./(Components)/Cross";
 import Faq from "./(homeComponent)/(FaqSection)/section";
 import Footer from "./(Components)/Footer";
+import WhyUs from "./(homeComponent)/Whyus";
 
 export default function Home() {
   return (
     <main className="mt-[100px]">
       <Banner />
       <OurServices />
+      <WhyUs />
       <Faq />
       <Blogs />
       <Cross />
