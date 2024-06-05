982699b53be5e8d66f27e1bf062c6f0cdf58ea68
Mankaran Singh <mankaransingh39@gmail.com>
2024-06-05 12:58:50 +0530

    Added:Certifate

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,14 +3,16 @@ import AboutBanner from "./AboutBanner";
 import OurJourney from "./OurJourney";
 import Brands from "./Brands";
 import Accordion from "./(accordion)/Accordion";
+import Certificate from "./Certificate";
 
 const page = () => {
   return (
     <div>
-      <AboutBanner />
-      <OurJourney />
-      <Accordion />
-      <Brands />
+      {/* <AboutBanner /> */}
+      {/* <OurJourney /> */}
+      {/* <Accordion /> */}
+      {/* <Brands /> */}
+      <Certificate />
     </div>
   );
 };
