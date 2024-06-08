56688d908eb3addfc6f31b0517821916023da76f
Mankaran Singh <mankaransingh39@gmail.com>
2024-06-08 14:46:07 +0530

    Fixed Navbar

--- src/app/(Components)/Nav.jsx
+++ src/app/(Components)/Nav.jsx
@@ -9,14 +9,14 @@ const Nav = () => {
       <div className="flex justify-between items-center">
         <Image src={logo} alt="logo" className="w-[5.521vw] h-[4.167vw]" />
         <div className="text-[1.25vw] font-normal flex gap-[3.49vw] py-auto items-center">
-          <Link href="">Portfolio</Link>
+          <Link href="">Home</Link>
+          <Link href="">About Us</Link>
           <Link href="">Services</Link>
-          <Link href="">Premium Services</Link>
-          <Link href="">Vision</Link>
-          <Link href="">Pricing</Link>
+          <Link href="">Portfolio</Link>
+          <Link href="">Insights</Link>
           <Link href="">Blogs</Link>
-          <button className="py-[0.729vw] px-[1.823] bg-[#60E2FF] text-white rounded-[5px] w-[10.417vw]">
-            Contact Us
+          <button className="py-[0.729vw] px-[1.823] bg-[#60E2FF] text-white rounded-[3.906vw] w-[10.417vw]">
+            Get In Touch
           </button>
         </div>
       </div>
