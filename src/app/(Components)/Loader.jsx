ce9b15f47662e190dfa5e193187d0621e9a5782f
unknown <you@example.com>
2024-07-04 15:17:12 +0530

    loader component added

---
+++ src/app/(Components)/Loader.jsx
@@ -0,0 +1,19 @@
+// Loader.js
+import React from "react";
+import "../../../src/app/globals.css"; // Ensure your global styles are imported properly
+
+const Loader = () => {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-white z-50">
+      <div className="flex justify-center items-center space-x-2">
+        <div className="loader-dot bg-blue-500"></div>
+        <div className="loader-dot bg-red-500"></div>
+        <div className="loader-dot bg-green-500"></div>
+        <div className="loader-dot bg-yellow-500"></div>
+        <div className="loader-dot bg-purple-500"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/app/blog/[url]/page.jsx
+++ src/app/blog/[url]/page.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+"use client"
+import React, { useState, useEffect } from "react";
 import BlogPage from "./BlogPage";
+import Loader from "../../(Components)/Loader";
 
 const getData = async (url) => {
   const res = await fetch(
@@ -7,14 +9,33 @@ const getData = async (url) => {
   );
 
   if (!res.ok) {
-    throw new Error("Failed to get ticket!");
+    throw new Error("Failed to get blog!");
   }
   return res.json();
 };
 
-const BlogDetails = async ({ params }) => {
-  const data = await getData(params.url);
-  const blogData = data.blog[0];
+const BlogDetails = ({ params }) => {
+  const [blogData, setBlogData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchBlogData = async () => {
+      try {
+        const data = await getData(params.url);
+        setBlogData(data.blog[0]);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching blog data:", error);
+        setLoading(false);
+      }
+    };
+    fetchBlogData();
+  }, [params.url]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return <BlogPage data={blogData} />;
 };
 
