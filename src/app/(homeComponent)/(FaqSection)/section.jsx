74c4871401777ddc0bb46847f88ff5f31e0bc067
unknown <you@example.com>
2024-07-04 18:36:24 +0530

    minor error fix due to ec2

--- src/app/(homeComponent)/(FaqSection)/section.jsx
+++ src/app/(homeComponent)/(FaqSection)/section.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import data from "./data";
-import Accordian from "./accordian";
+import Accordian from "./Accordian";
 
 const Faq = () => {
   const [activeIndex, setActiveIndex] = useState(-1);
