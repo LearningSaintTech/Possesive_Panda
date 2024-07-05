86e681c9e3ac79bb8292d21d0d832eac9526b042
adityaa108 <aditya@learningsaint.com>
2024-07-05 01:07:16 -0700

    Added: about US

--- src/app/about/(accordion)/Accordion.jsx
+++ src/app/about/(accordion)/Accordion.jsx
@@ -1,13 +1,26 @@
 "use client";
 import React, { useState } from "react";
-import { accordian } from "../data";
+import {accordian} from "../data";
 import AccordionItems from "./AccordionItems";
 
 const Accordion = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0); // Initially open the first item
+  const [lastActiveIndex, setLastActiveIndex] = useState();
 
+  const handleMouseEnter = (index) => {
+    setActiveIndex(index);
+    setLastActiveIndex(index);
+  };
+
+  const handleMouseLeave = (index) => {
+   
+    if(lastActiveIndex != index){
+      setActiveIndex(null); 
+    }
+  };
+console.log(accordian)
   return (
-    <div className="bg-[#FAFAFA] flex flex-col mt-10 lg:mt-32 w-full py-14">
+    <div className='bg-[#FAFAFA] flex flex-col mt-10 lg:mt-32 w-full py-14'>
       {accordian?.map((item, index) => (
         <AccordionItems
           key={index}
@@ -15,8 +28,9 @@ const Accordion = () => {
           answer={item.answer}
           image={item.image}
           index={index}
-          activeIndex={activeIndex}
-          setActiveIndex={setActiveIndex}
+          isOpen={activeIndex === index}
+          onMouseEnter={() => handleMouseEnter(index)}
+          onMouseLeave={()=>handleMouseLeave(index)}
         />
       ))}
     </div>
