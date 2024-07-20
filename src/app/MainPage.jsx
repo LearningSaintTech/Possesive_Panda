5cc345ad2ef51eacabe85d17715e379058498f28
unknown <you@example.com>
2024-07-20 20:51:19 +0530

    seo integration

---
+++ src/app/MainPage.jsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import Banner from "./(homeComponent)/Banner";
+import OurServices from "./(homeComponent)/OurServices";
+import Blogs from "./(homeComponent)/Blogs";
+import Cross from "./(Components)/Cross";
+import Faq from "./(homeComponent)/(FaqSection)/section";
+import WhyUs from "./(homeComponent)/Whyus";
+import Technologies from "./(homeComponent)/(Technologies)/Technologies";
+import Portfolio from "./(homeComponent)/Portfolio";
+import Contact from "./(Components)/Contact";
+import Loader from "./(Components)/Loader"; // Adjust the path accordingly
+
+const MainPage = () => {
+  const whyUsRef = useRef(null);
+  const [whyUsHeight, setWhyUsHeight] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (whyUsRef.current) {
+      setWhyUsHeight(whyUsRef.current.offsetTop);
+    }
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000); // 1-second delay
+
+    // Clean up the timer
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Banner whyUsHeight={whyUsHeight} />
+          <Portfolio />
+          <OurServices />
+          <div ref={whyUsRef}>
+            <WhyUs />
+          </div>
+          <Technologies />
+          <Contact />
+          <Blogs />
+          <Faq />
+          <Cross />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MainPage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,37 +1,123 @@
-"use client";
-import Banner from "./(homeComponent)/Banner";
-import OurServices from "./(homeComponent)/OurServices";
-import Blogs from "./(homeComponent)/Blogs";
-import Cross from "./(Components)/Cross";
-import Faq from "./(homeComponent)/(FaqSection)/section";
-import WhyUs from "./(homeComponent)/Whyus";
-import Technologies from "./(homeComponent)/(Technologies)/Technologies";
-import Portfolio from "./(homeComponent)/Portfolio";
-import Contact from "./(Components)/Contact";
-import { useEffect, useRef, useState } from "react";
+import React from 'react';
+import Head from 'next/head';
+import MainPage from './MainPage';
+import Script from "next/script";
 
-export default function Home() {
-  const whyUsRef = useRef(null);
-  const [whyUsHeight, setWhyUsHeight] = useState(0);
+export const metadata = {
+  title: 'Leading Software Development Company - Possessive Panda',
+  description: 'Possessive Panda is a global IT service company in the US, UAE & UK. We offer digital marketing, mobile application development, web design & development and more to startups and enterprises clients across.',
+  alternates: {
+    canonical: 'https://www.possesivepanda.com'
+  },
+  openGraph: {
+    title: 'Leading Software Development Company - Possessive Panda',
+    siteName: 'Possessive Panda',
+    url: 'https://www.possesivepanda.com/',
+    description: 'Possessive Panda is a global IT service company in the US, UAE & UK. We offer digital marketing, mobile application development, web design & development and more to startups and enterprises clients across.',
+    type: 'website',
+    images: [
+      {
+        url: 'https://www.possesivepanda.com/path-to-image.jpg',
+        
+      },
+    ],},
+  breadcrumbList: {
+    "@context": "https://schema.org/",
+    "@type": "BreadcrumbList",
+    "itemListElement": [
+      { "@type": "ListItem", "position": 1, "name": "Possessive Panda", "item": "https://possesivepanda.com/" },
+      { "@type": "ListItem", "position": 2, "name": "About", "item": "https://possesivepanda.com/about" },
+      { "@type": "ListItem", "position": 3, "name": "Portfolio", "item": "https://possesivepanda.com/portfolio" },
+      { "@type": "ListItem", "position": 4, "name": "Contact Us", "item": "https://possesivepanda.com/contact" },
+      { "@type": "ListItem", "position": 5, "name": "Blog", "item": "https://possesivepanda.com/blog" }
+    ]
+  },
+  organization: {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "Possessive Panda",
+    "url": "https://possesivepanda.com/",
+    "logo": "https://possesivepanda.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnav-logo.8084e162.png&w=384&q=75",
+    "sameAs": [
+      "https://www.facebook.com/people/Possessive-Panda/61559502103126/",
+      "https://www.instagram.com/possesivepanda/",
+      "https://www.linkedin.com/company/102720317/admin/dashboard/",
+      "https://x.com/possesivepanda",
+      "https://possesivepanda.com/"
+    ]
+  },
+  faq: {
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    "mainEntity": [
+      { "@type": "Question", "name": "What technologies are used in web development?", "acceptedAnswer": { "@type": "Answer", "text": "HTML, CSS, JavaScript, PHP, React, and Angular are some technologies used in web development. We use many more to deliver services based on clients’ needs." }},
+      { "@type": "Question", "name": "Does Possessive Panda provide services for startups?", "acceptedAnswer": { "@type": "Answer", "text": "Absolutely, yes. We have Startup Bundle Services for aspiring entrepreneurs and startup businesses, through which they can achieve great heights." }},
+      { "@type": "Question", "name": "How can your IT services benefit our business?", "acceptedAnswer": { "@type": "Answer", "text": "Our IT services can help improve your business operations, efficacy, and satisfaction of customers. We also help maintain security and effectively achieve digital goals." }},
+      { "@type": "Question", "name": "Is there any Customer support available here?", "acceptedAnswer": { "@type": "Answer", "text": "Yes, our customers are our priority. Our customer support is always available to handle customer queries and provide timely phone, email, and chat assistance." }},
+      { "@type": "Question", "name": "What services does Possessive Panda offer?", "acceptedAnswer": { "@type": "Answer", "text": "Possessive Panda offers multiple IT services, such as software development, app development, cloud services, web development, and digital marketing. We also provide a range of services to suit your business needs." }},
+      { "@type": "Question", "name": "How do you guarantee the security of my business data?", "acceptedAnswer": { "@type": "Answer", "text": "The security of your business data is our responsibility. We implement standard cybersecurity measures, such as encryption, firewalls, intrusion detection systems, and regular security audits in accordance with data protection regulations." }}
+    ]
+  },
+ 
+};
 
-  useEffect(() => {
-    if (whyUsRef.current) {
-      setWhyUsHeight(whyUsRef.current.offsetTop);
-    }
-  }, []);
-  return (
-    <main className="">
-      <Banner whyUsHeight={whyUsHeight} />
-      <Portfolio />
-      <OurServices />
-      <div ref={whyUsRef}>
-        <WhyUs />
-      </div>
-      <Technologies />
-      <Contact />
-      <Blogs />
-      <Faq />
-      <Cross />
-    </main>
-  );
-}
+const Home = () => (
+  <main>
+    <Head>
+      <title>{metadata.title}</title>
+      <meta name="description" content={metadata.description} />
+      <link rel="canonical" href={metadata.canonical} />
+
+ {/* <Script type="application/ld+json">
+        {JSON.stringify(metadata.breadcrumbList)}
+      </Script>
+
+      <Script type="application/ld+json">
+        {JSON.stringify(metadata.organization)}
+      </Script>
+
+      <Script type="application/ld+json">
+        {JSON.stringify(metadata.faq)}
+      </Script> */}
+      {/* <script async src={`https://www.googletagmanager.com/gtag/js?id=${metadata.googleAnalytics.id}`}></script>
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${metadata.googleAnalytics.id}');
+        `}
+      </script>
+
+      <meta name="google-site-verification" content={metadata.googleSearchConsole.verification} /> */}
+
+      {/* Open Graph Meta Tags */}
+      <meta property="og:title" content={metadata.openGraph.title} />
+      <meta property="og:site_name" content={metadata.openGraph.siteName} />
+      <meta property="og:url" content={metadata.openGraph.url} />
+      <meta property="og:description" content={metadata.openGraph.description} />
+      <meta property="og:type" content={metadata.openGraph.type} />
+      <meta property="og:image" content={metadata.openGraph.images[0].url} />
+
+     
+    </Head>
+     {/* Breadcrumb List Schema Markup */}
+     <Script type="application/ld+json">
+        {JSON.stringify(metadata.breadcrumbList)}
+      </Script>
+
+      {/* Organization Schema Markup */}
+      <Script type="application/ld+json">
+        {JSON.stringify(metadata.organization)}
+      </Script>
+
+      {/* FAQ Schema Markup */}
+      <Script type="application/ld+json">
+        {JSON.stringify(metadata.faq)}
+      </Script>
+
+    <MainPage />
+  </main>
+);
+
+export default Home;
