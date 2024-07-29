b24b322f1e6adee5642416945f5e3e7b0d132710
Arya Singh <aryakumarsingh191@gmail.com>
2024-07-29 13:15:07 +0530

    final push

--- src/app/layout.js
+++ src/app/layout.js
@@ -57,10 +57,10 @@ export default function RootLayout({ children }) {
               fbq('track', 'PageView');
             `,
           }}
-        />
+        /> 
         <noscript><img height="1" width="1" style="display:none"
-              src="https://www.facebook.com/tr?id=380882334679792&ev=PageView&noscript=1"
-               /></noscript>
+          src="https://www.facebook.com/tr?id=380882334679792&ev=PageView&noscript=1"
+        /></noscript>
       </head>
       <body className={poppins.className}>
         <Nav />
