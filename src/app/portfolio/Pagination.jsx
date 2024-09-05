f60155fcc690e9d128cfd659fa265572dbe32aef
akhilmishra365 <akhilmishra1676@gmail.com>
2024-09-05 17:39:59 +0530

    Blog Page Pagination

---
+++ src/app/portfolio/Pagination.jsx
@@ -0,0 +1,50 @@
+// Pagination.js
+import React from 'react';
+import Line9 from '../../../public/assets/Line9.svg'
+import Image from 'next/image';
+
+const Pagination = ({ totalPosts, postsPerPage, setCurrentPage, currentPage }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div className='flex flex-col mb-10 mt-5 justify-center items-center'>
+    <div className="flex items-center justify-center mt-4 space-x-2 mb-[3.49vw]">
+      
+      <button
+        className="px-2 py-1 bg-[#00111A] text-white rounded border border-white disabled:opacity-50 justify-center items-center"
+        onClick={() => setCurrentPage(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        &lt;
+      </button>
+      {pageNumbers.map((number) => (
+        <button
+          key={number}
+          onClick={() => setCurrentPage(number)}
+          className={`px-3 py-1 rounded  border border-white justify-center items-center  ${
+            currentPage === number ? 'bg-[#60E2FF] text-white' : 'bg-[#00111A] text-white'
+          }`}
+        >
+          {number}
+        </button>
+      ))}
+      <button
+        className="px-2 py-1  bg-[#00111A] text-white rounded border border-white disabled:opacity-50 justify-center items-center mt"
+        onClick={() => setCurrentPage(currentPage + 1)}
+        disabled={currentPage === pageNumbers.length}
+      >
+        &gt;
+      </button>
+   
+ 
+    </div>
+    <Image src={Line9} className='w-[64.635vw] '/>
+    </div>
+  );
+};
+ 
+export default Pagination;
