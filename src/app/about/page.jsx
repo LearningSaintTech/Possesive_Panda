d29fbd180cb0320ab94cf6eb05ee74007499616c
adityaa108 <aditya@learningsaint.com>
2024-09-06 05:53:44 -0700

    Added Journey and  marque

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,14 +6,14 @@ import Certificate from "./Certificate";
 import NewBanner from "../(Components)/NewBanner";
 // import Content from "./Content";
 import Footer from "../(Components)/Footer";
-import AboutMarquee from "./AboutMarquee";
+import AboutMarquee from "../(Components)/AboutMarquee";
 import Values from "./Values";
 import Success from "./Success";
 import Intro from "../about/Intro";
 import Review from "./Review";
 import ReviewSlider from "./ReviewSlider";
 import Purpose from "../about/Purpose";
-import Journey from "../contact/Journey";
+import Journey from "../(Components)/Journey";
 
 
 const page = () => {
@@ -26,13 +26,17 @@ const page = () => {
       // image={banner}
       />
       <AboutMarquee />
-      <Journey />
+      <Journey
+        title="Our journey started"
+        subtitle="Our journey began with your success - Your achievements are the fuel that drives our excellence and dedication."
+        description="Possessive Panda is a global powerhouse, renowned for its innovation and expertise. Our diverse team, from around the world, brings varied knowledge and perspectives. This cultural richness fuels our cutting-edge solutions and industry leadership. Our commitment to excellence keeps us at the forefront, meeting clients' evolving needs globally."
+      />
       <Intro />
       <Purpose />
       {/* <Accordion /> */}
       <Values />
       <Review />
-      <ReviewSlider/>
+      <ReviewSlider />
       <Technologies />
       <Certificate />
       <Success />
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,13 @@
+"use client"
 import React from "react";
-import Journey from "./Journey";
 import Joinpanda from "./Joinpanda";
 import NewBanner from "../(Components)/NewBanner";
 import Contact from "../(Components)/Contact";
 import banner from "../../assets/banners/contact-us.png";
 import Footer from "../(Components)/Footer";
 import Faq from "../(homeComponent)/(FaqSection)/section";
+import Journey from "../(Components)/Journey";
+import AboutMarquee from "../(Components)/AboutMarquee";
 
 
 
@@ -17,7 +19,12 @@ const page = () => {
         desciption={"Possesive Panda revolutionized businesses by providing innovative, tech-driven solutions, serving numerous companies to stay ahead in the market."}
       // image={banner}
       />
-      <Journey />
+      <AboutMarquee/>
+      <Journey
+        title="Like discussing your ideas in person?"
+        subtitle="Swing by our office anytime! We're here to turn your tech dreams into reality and ensure you leave with a smile."
+        description="Every project begins with an email or phone call, regardless of whether you need a basic website, a mobile app, custom software, virtual assistance services, or to establish your own development team. You are just a call or an email away from being the pinnacle of your industry."
+      />
       <Joinpanda />
       <Contact />
       <Faq />
