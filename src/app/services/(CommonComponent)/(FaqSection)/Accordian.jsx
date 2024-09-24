2fd2280038deacfa280a8c810067f662c21fde63
unknown <you@example.com>
2024-09-24 19:14:00 +0530

    fix the bug

--- src/app/services/(CommonComponent)/(FaqSection)/Accordian.jsx
+++ src/app/services/(CommonComponent)/(FaqSection)/Accordian.jsx
@@ -2,29 +2,36 @@
 import React, { useRef } from "react";
 
 const Accordian = ({ title, children, index, activeIndex, setActiveIndex }) => {
-  const handleSetIndex = (index) => {
+  const isOpen = activeIndex === index; // Check if the current accordion should be open
+  const accordianRef = useRef(null); // Reference for the accordion
+
+  // Handle mouse enter event to set the active index
+  const handleMouseEnter = () => {
+    // If the hovered index is not the current active index, set the active index to the hovered index
     if (activeIndex !== index) {
       setActiveIndex(index);
-    } else {
+    }
+  };
+
+  // Handle mouse leave event
+  const handleMouseLeave = () => {
+    // Close the accordion if the current index is not the first one
+    if (index !== 1) {
       setActiveIndex(null);
     }
   };
 
-  const isOpen = activeIndex === index;
-  const accordianRef = useRef(null);
-
   return (
     <div
-      onClick={() => handleSetIndex(index)} // Click handler on the parent div
-      className={`flex cursor-pointer flex-row rounded-[0.625vw] bg-[#131D22] mb-[1.198vw] pl-[1.25vw] pr-[1.667vw] pt-[2.448vw] sm:pb-[2.448vw] pb-[1vw] relative transition-shadow duration-[1.2s] ease-in-out`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className={`flex cursor-pointer flex-row rounded-[0.625vw] bg-[#131D22] sm:mb-[1.198vw] sm:pl-[1.25vw] sm:pr-[1.667vw] sm:pt-[2.448vw] sm:pb-[2.448vw] mb-[4.198vw] pl-[4.25vw] pr-[4.667vw] pt-[4.448vw] pb-[4vw] relative transition-shadow duration-[1.2s] ease-in-out`}
       style={{
-        boxShadow: isOpen
-          ? "0 10px 8px rgba(255, 255, 255, 0.3)" // White shadow
-          : "none",
+        boxShadow: isOpen ? "0 10px 8px rgba(255, 255, 255, 0.3)" : "none", // Shadow effect based on open state
       }}
     >
       <div className="flex flex-col w-full">
-        <div className="flex  justify-between">
+        <div className="flex justify-between">
           <p className="text-[#FFF] sm:text-[1.25vw] text-[3.294vw] sm:font-medium font-normal capitalize">
             {title}
           </p>
@@ -32,11 +39,11 @@ const Accordian = ({ title, children, index, activeIndex, setActiveIndex }) => {
         <div
           className={`overflow-hidden transition-all duration-[1.2s] ease-in-out`}
           style={{
-            maxHeight: isOpen ? `200px` : "0px",
+            maxHeight: isOpen ? `200px` : "0px", // Set max height based on open state
           }}
         >
           <div
-            className="w-full sm:w-[29.583vw]  sm:mt-[1.188vw] opacity-[0.6] ml-[10px] sm:ml-[1.25vw] text-white text-[14px] sm:text-[1.042vw] font-normal flex items-start"
+            className="w-full sm:w-[29.583vw] sm:mt-[1.188vw] opacity-[0.6] ml-[10px] sm:ml-[1.25vw] text-white text-[14px] sm:text-[1.042vw] font-normal flex items-start"
             ref={accordianRef}
           >
             <ul>
@@ -49,24 +56,27 @@ const Accordian = ({ title, children, index, activeIndex, setActiveIndex }) => {
         className="flex items-center justify-center transition-transform duration-300 ease-in-out"
         style={{
           transform: isOpen ? "translateY(42%) rotate(180deg)" : "rotate(0)",
-          transition: "transform 0.7s ease-in-out",
         }}
       >
-       <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none"
-
-          className="transition-transform sm:block hidden duration-300 ease-in-out"
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          fill="none"
+          className="transition-transform sm:block lg:h-[1.529vw] lg:w-[1.529vw] md:h-[2vw] md:w-[2vw] hidden duration-300 ease-in-out"
         >
-         <path d="M28 10L16 22L4 10" stroke="white" stroke-width="4" stroke-linecap="round"/>
-
+          <path d="M28 10L16 22L4 10" stroke="white" strokeWidth="4" strokeLinecap="round" />
         </svg>
-
-
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none"
-
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+          fill="none"
           className="transition-transform sm:hidden block duration-300 ease-in-out"
-
         >
-          <path d="M15.4961 5.53369L8.99475 12.035L2.4934 5.53369" stroke="white" stroke-width="2.16712" stroke-linecap="round" />
+          <path d="M15.4961 5.53369L8.99475 12.035L2.4934 5.53369" stroke="white" strokeWidth="2.16712" strokeLinecap="round" />
         </svg>
       </div>
     </div>
--- src/app/services/(CommonComponent)/Platforms.jsx
+++ src/app/services/(CommonComponent)/Platforms.jsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 
 export const Platforms = ({ platformsData }) => {
   return (
-    <div className='px-[9vw] py-[5vw] text-left bg-[#00111A] gap-[10.99vw] flex sm:flex-row flex-col'>
-      <div className='flex mt-[3.4vw] gap-[0.5vw] flex-col ml-[3vw]'>
-        <h2 className='sm:w-[33.906vw] w-[85.882vw] mt-[1.719vw] sm:items-start justify-center sm:text-start text-center items-center text-white sm:text-[2.5vw] text-[7.529vw] font-semibold leading-[normal]'>
+    <div className='px-[9vw] py-[5vw] text-left bg-[#00111A] justify-center items-center gap-[10.99vw] flex sm:flex-row flex-col'>
+      <div className='flex  gap-[0.5vw] flex-col ml-[3vw]'>
+        <h2 className='sm:w-[33.906vw] w-[85.882vw]  sm:items-start justify-center sm:text-start text-center items-center text-white sm:text-[2.5vw] text-[7.529vw] font-semibold leading-[normal]'>
           {platformsData[0].text.heading}
         </h2>
         <p className='sm:w-[33.906vw] w-[85.882vw] text-white m:items-start justify-center sm:text-start text-center items-center text-[3.765vw] sm:text-[1.25vw]'>
@@ -16,7 +16,7 @@ export const Platforms = ({ platformsData }) => {
         {platformsData[0].images.map((platform, index) => (
           <Image
             key={index}
-            className='hover:scale-75 transition-transform duration-700 cursor-pointer'
+            className='hover:scale-75 sm:h-[7.708vw] sm:w-[7.708vw] h-[26.118vw] w-[26.118vw] transition-transform duration-700 cursor-pointer'
             src={platform.imageSrc}
             alt={platform.altText}
           />
