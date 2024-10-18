523b27331c5798809f85eadc6d0ba024a019357a
unknown <you@example.com>
2024-10-19 00:14:54 +0530

    meta tag added

--- src/app/webinar/page.jsx
+++ src/app/webinar/page.jsx
@@ -10,6 +10,7 @@ import SeventhComp from './SeventhComp';
 import EightethComp from './EightethComp';
 import Footer from '../(Components)/Footer';
 import Timer3 from './Timer';
+import Head from 'next/head'; 
 
 const Page = () => {
     const thirdCompRef = useRef(null);
@@ -25,6 +26,33 @@ const Page = () => {
 
     return (
         <div>
+            <Head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            !function(f,b,e,v,n,t,s)
+                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                            n.queue=[];t=b.createElement(e);t.async=!0;
+                            t.src=v;s=b.getElementsByTagName(e)[0];
+                            s.parentNode.insertBefore(t,s)}(window, document,'script',
+                            'https://connect.facebook.net/en_US/fbevents.js');
+                            fbq('init', '536379559281294');
+                            fbq('track', 'PageView');
+                        `,
+                    }}
+                />
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: 'none' }}
+                        src="https://www.facebook.com/tr?id=536379559281294&ev=PageView&noscript=1"
+                        alt="fb-pixel"
+                    />
+                </noscript>
+            </Head>
             <BannerComp scrollToThirdComp={scrollToThirdComp} />
             <Timer3/>
             <SecondComp />
